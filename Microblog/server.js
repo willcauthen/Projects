c47e7ad5c8468a1f8a7900de9823616221e23214
Will Cauthen <willcauthen50@gmail.com>
2015-10-17 14:29:33 -0700

    project one restored from server

---
+++ Microblog/server.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyparser = require('body-parser');
+
+// config codes
+app.set ('view engine', 'ejs');
+
+//express.static: app.use(express: static('public'));
+
+app.use("/static", express.static("public"));
+
+
+app.get('/', function(request, response) {
+	response.render('index');
+});
+
+app.get('/', function(request, response) {
+	response.render('styles');
+});
+
+app.get('/', function(request, response) {
+	response.send('It is working');
+});
+
+app.listen(4000, function(request, response) {
+	console.log("Listening at localhost:4000");
+});
